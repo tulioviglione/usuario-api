e451cfa776881d9d04c27ae83b483d83f2aa7705
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-07-09 19:55:26 -0300

    Commit inicial

---
+++ src/main/java/com/usuario/api/UsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.usuario.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioApiApplication.class, args);
+	}
+
+}
